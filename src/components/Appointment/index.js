8cfbbd4a7b8add7128fd3c4f7c69806206c888b4
kmomozin <khinmomozin@gmail.com>
2023-02-22 19:05:43 -0700

    show appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,8 +4,17 @@ import Header from 'components/Appointment/Header.js';
 import Empty from 'components/Appointment/Empty.js';
 import Show from 'components/Appointment/Show.js';
 
-export default function Appointment(porps) {
+export default function Appointment(props) {
+  const { id, time, interview } = props;
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={time} />
+      {interview &&
+        <Show
+          student={interview.student}
+          interviewer={interview.interviewer.name}
+        />}
+      {!interview && <Empty />}
+    </article>
   );
 }
