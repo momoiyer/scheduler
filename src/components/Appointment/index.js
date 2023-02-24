8dd0e22b0d7f3d6e5a2692ca453253bb053ffe60
kmomozin <khinmomozin@gmail.com>
2023-02-24 00:23:06 -0700

    use custom hook to render appropiate appointment child components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,19 +3,46 @@ import 'components/Appointment/styles.scss';
 import Header from 'components/Appointment/Header.js';
 import Empty from 'components/Appointment/Empty.js';
 import Show from 'components/Appointment/Show.js';
+import Form from 'components/Appointment/Form.js';
+import useVisualMode from 'hooks/useVisualMode';
 
 export default function Appointment(props) {
   const { id, time, interview } = props;
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const CONFIRM = "CONFIRM";
+  const CANCEL = "CANCEL";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview &&
+      {mode === EMPTY &&
+        <Empty
+          onAdd={() => transition(CREATE)}
+        />
+      }
+      {mode === SHOW && (
         <Show
           key={id}
           student={interview.student}
           interviewer={interview.interviewer.name}
-        />}
-      {!interview && <Empty />}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          student=""
+          interviewer={3}
+          interviewers={[]}
+          onSave={() => transition(CONFIRM)}
+          onCancel={back}
+        />
+      )}
     </article>
   );
 }
